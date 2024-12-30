63685c5daed33bc2d2b31fcf0f1e5bb1e57733b9
ManuelQuijada <manuelquijada2000@gmail.com>
2024-12-30 11:42:47 +0100

    springsecurity implementation

--- src/main/java/com/paymentchain/billing/InvoiceApplication.java
+++ src/main/java/com/paymentchain/billing/InvoiceApplication.java
@@ -5,10 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class InvoiceApplication {
 
 	public static void main(String[] args) {
